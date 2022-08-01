80ac937e05fce8adf4c160a12ef805d36f9abb3b
Danilo Yamashita <dyamash2@its.jnj.com>
2022-08-01 15:01:42 -0300

    Intelephense

--- plugin/intelephense.lua
+++ plugin/intelephense.lua
@@ -1,3 +1,15 @@
+require("nvim-lsp-installer").setup {
+  automatic_installation = true
+}
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -25,25 +37,39 @@ local on_attach = function(client, bufnr)
 end
 
 local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
 }
 
-require('lspconfig')['intelephense'].setup {
-  on_attach = on_attach,
-  flags = lsp_flags,
+local lspconfig = require("lspconfig")
+
+
+lspconfig.intelephense.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
+lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
   settings = {
-    files = {
-      maxSize = 10000000;
-      associations = {
-        "*.php",
-        "*.phtml",
-        "*.inc",
-        "*.module",
-        "*.install",
-        "*.theme",
-        "*.test",
-      };
-    };
-  }
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
