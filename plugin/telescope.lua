824931840e60d70f3d814f729b84e0b3d598b791
Danilo Yamashita <dyamash2@its.jnj.com>
2022-08-01 08:49:22 -0300

    Global search

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,4 +1,5 @@
--- Ctrl-p
+-- Open file: Ctrl-p
 vim.api.nvim_set_keymap("n", "<c-p>", ':Telescope find_files<CR>', {noremap=true, silent=true})
--- \-f
-vim.api.nvim_set_keymap("n", "<Leader>f", ':Telescope live_grep<CR>', {noremap=true, silent=true})
+
+-- Global Search: Ctrl-Shift-f
+vim.api.nvim_set_keymap("n", "<C-f>", ':Telescope live_grep<CR>', {noremap=true, silent=true})
