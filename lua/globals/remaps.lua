89e429630700d8680aa88dfdbf792cf80cbec063
Danilo Yamashita <dyamash2@its.jnj.com>
2022-08-01 10:56:54 -0300

    File Explorer

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -1,3 +1,6 @@
 -- Ctrl+S for saving
 vim.api.nvim_set_keymap("n", "<c-s>", '<esc>:w<CR>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap("i", "<c-s>", '<esc>:w<CR>i', {noremap=true, silent=true})
+
+-- File Explorer
+vim.api.nvim_set_keymap("n", "-", '<esc>:Explore<CR>', {noremap=true, silent=true})
