ed621f417eb61918ec7a448e7c95b82dc9b1d2a4
Danilo Yamashita <dyamash2@its.jnj.com>
2022-08-01 11:04:24 -0300

    Ctrl-d to duplicate line

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -4,3 +4,7 @@ vim.api.nvim_set_keymap("i", "<c-s>", '<esc>:w<CR>i', {noremap=true, silent=true
 
 -- File Explorer
 vim.api.nvim_set_keymap("n", "-", '<esc>:Explore<CR>', {noremap=true, silent=true})
+
+-- Duplicate line
+vim.api.nvim_set_keymap("n", "<c-d>", '<S-v>yp', {noremap=true, silent=true})
+vim.api.nvim_set_keymap("v", "<c-d>", 'ygv<esc>p', {noremap=true, silent=true})
