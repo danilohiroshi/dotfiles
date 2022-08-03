6b31b8a3f32c2bb67dfd82b6bdb714734de57b47
Danilo Yamashita <dyamash2@its.jnj.com>
2022-08-03 11:25:43 -0300

    Remove ctrl-o

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -5,10 +5,7 @@ vim.api.nvim_set_keymap("n", "<c-p>", ':Telescope find_files<CR>', {noremap=true
 vim.api.nvim_set_keymap("n", "<C-f>", ':Telescope live_grep<CR>', {noremap=true, silent=true})
 
 -- Open Old files
-vim.api.nvim_set_keymap("n", "<c-o>", ':Telescope oldfiles<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<c-e>", ':Telescope oldfiles<CR>', {noremap=true, silent=true})
 
 -- Git status
 vim.api.nvim_set_keymap("n", "<c-g>", ':Telescope git_status<CR>', {noremap=true, silent=true})
-
--- Buffers
-vim.api.nvim_set_keymap("n", "<c-e>", ':Telescope buffers<CR>', {noremap=true, silent=true})
