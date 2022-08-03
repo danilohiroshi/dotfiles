51216785dcc747503944dacfe96ce3513fdf2c22
Danilo Yamashita <dyamash2@its.jnj.com>
2022-08-03 09:03:25 -0300

    Window split

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -23,4 +23,8 @@ vim.api.nvim_set_keymap("i", "<c-z>", '<esc>ui', opts)
 vim.api.nvim_set_keymap("n", "<c-z>", 'u', opts)
 
 -- Window split
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
 
