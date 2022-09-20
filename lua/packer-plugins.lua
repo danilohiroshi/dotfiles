3211f88890923737d5e550599a9d3fdc9f5c0282
Danilo Yamashita <dyamash2@its.jnj.com>
2022-09-20 14:30:04 -0300

    updates

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -10,7 +10,6 @@ return require('packer').startup(function()
   -- Dependency, install https://lindevs.com/install-ripgrep-on-ubuntu/
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-telescope/telescope-live-grep-args.nvim'
   use 'nvim-telescope/telescope-symbols.nvim'
 
@@ -31,26 +30,11 @@ return require('packer').startup(function()
   -- Buffer line
   use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
 
-  -- Git integrations
-  use {'lewis6991/gitsigns.nvim'}
-
-  -- File tree
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
-
   -- Multicursor
   use {'terryma/vim-multiple-cursors'}
 
   -- Surround
   use{'kylechui/nvim-surround'}
-
-  -- Treesitter
-  use {'nvim-treesitter/nvim-treesitter'}
 
   -- Spell check
   use {'lewis6991/spellsitter.nvim'}
