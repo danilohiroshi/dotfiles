03184364b0f73530c02d6e4c58838f1b4a3362c5
Danilo Yamashita <dyamash2@its.jnj.com>
2022-12-20 13:52:28 -0300

    Bufferline

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -1,2 +1,8 @@
 vim.opt.termguicolors = true
-require("bufferline").setup {}
+require("bufferline").setup {
+  options = {
+    mode = "buffers",
+    truncate_names = false,
+    separator_style = "thin"
+  }
+}
