6cc78be620cc33fea1e43aa8ade41fdca7cced57
Yamashita, Danilo <dyamash2@its.jnj.com>
2022-12-29 11:47:13 -0300

    nvim tree

--- plugin/tree.lua
+++ plugin/tree.lua
@@ -18,6 +18,20 @@ require('nvim-tree').setup({
       -- inline_arrows = false,
     },
   },
+  view = {
+    float = {
+      enable = true,
+      quit_on_focus_loss = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 60,
+        height = 60,
+        row = 1,
+        col = 1,
+      }
+    }
+  }
 })
 
 vim.cmd([[
