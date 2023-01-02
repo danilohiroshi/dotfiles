c0827ec6d576774de8bac9013ada38be786178d8
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-01-02 14:14:25 -0300

    Debugger

---
+++ plugin/debugger.lua
@@ -0,0 +1,7 @@
+
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap("n", "<F5>", ':DapContinue<cr>', opts)
+vim.api.nvim_set_keymap("n", "<F8>", ':DapStepOver<cr>', opts)
+vim.api.nvim_set_keymap("n", "<F7>", ':DapStepInto<cr>', opts)
+vim.api.nvim_set_keymap("n", "<S-F5>", ':DapTerminate<cr>', opts)
+vim.api.nvim_set_keymap("n", "<F10>", ':DapToggleBreakpoint<cr>', opts)
