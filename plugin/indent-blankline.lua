cb0a838b0091709ec462aabaeb3277a745abdde6
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-01-03 14:16:06 -0300

    Indent guide

--- plugin/indent-blankline.lua
+++ plugin/indent-blankline.lua
@@ -16,5 +16,5 @@ require('indent_blankline').setup({
   show_trailing_blankline_indent = false,
   show_first_indent_level = true,
   show_current_context = true,
-  show_current_context_start = true,
+  show_current_context_start = false,
 })
