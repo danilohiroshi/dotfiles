75a5030dde7ff264c3a96a49cb958250d273589d
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-01-04 16:57:21 -0300

    Do not show contextual guide

--- plugin/indent-blankline.lua
+++ plugin/indent-blankline.lua
@@ -15,6 +15,6 @@ require('indent_blankline').setup({
   },
   show_trailing_blankline_indent = false,
   show_first_indent_level = true,
-  show_current_context = true,
+  show_current_context = false,
   show_current_context_start = false,
 })
