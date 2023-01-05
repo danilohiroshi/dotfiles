93225e6fe0a187816f815e89b2f06fcb8ce90067
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-01-05 15:55:33 -0300

    jshint version

--- plugin/nullls.lua
+++ plugin/nullls.lua
@@ -3,7 +3,11 @@ local null_ls = require("null-ls")
 -- - Supported linters: https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 null_ls.setup({
     sources = {
-      null_ls.builtins.diagnostics.jshint,
+      null_ls.builtins.diagnostics.jshint.with({
+        extra_args = {
+            "--config $HOME/.config/nvim/jshint/.jshintrc",
+          }
+      }),
       null_ls.builtins.diagnostics.tsc,
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.diagnostics.phpcs.with({
