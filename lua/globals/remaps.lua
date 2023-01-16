55053730746b990cd5b8bbec6735dadb7fb0be4b
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-01-16 14:06:44 -0300

    Remaps

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -38,8 +38,6 @@ vim.api.nvim_set_keymap("n", "<c-z>", 'u', opts)
 
 -- Window split
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Reselect visual selection after indenting.
