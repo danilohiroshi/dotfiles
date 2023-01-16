e6d788c96dfdd16db6b55e23d87b97a254ce8b14
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-01-16 17:10:32 -0300

    Configs

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -8,18 +8,6 @@ return require('packer').startup(function()
     'navarasu/onedark.nvim',
     config = function()
       vim.cmd('colorscheme onedark')
-      -- vim.api.nvim_set_hl(0, 'FloatBorder', {
-      --   fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-      --   bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-      -- })
-
-      -- -- Make the cursor line background invisible
-      -- vim.api.nvim_set_hl(0, 'CursorLineBg', {
-      --   fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-      --   bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-      -- })
-
-      -- vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
     end,
   })
 
