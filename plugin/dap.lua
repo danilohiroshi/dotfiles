5473a1291857a1718469b107b4e839970dee5d0a
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-02-24 09:00:16 -0300

    Debug

---
+++ plugin/dap.lua
@@ -0,0 +1,5 @@
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<F5>", ":lua require'dap'.continue()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<F7>", ":lua require'dap'.step_into()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<F8>", ":lua require'dap'.step_over()<cr>", opts)
