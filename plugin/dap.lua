9111dd4087f44c885ac752bfef26f47d9d9184a8
Yamashita, Danilo <dyamash2@its.jnj.com>
2023-02-24 10:24:17 -0300

    Debug

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -18,6 +18,28 @@ dap.configurations.typescript = {
   }
 }
 
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
+  },
+}
+
+dap.defaults.fallback.terminal_win_cmd = 'tabnew'
+
 local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<cr>", opts)
 vim.api.nvim_set_keymap("n", "<F6>", ":lua require'dap'.terminate()<cr>", opts)
