d2c6ec41e90d7d53a5cc9efa142323a44f2696de
Yamashita, Danilo <dyamash2@its.jnj.com>
2024-01-18 14:54:24 -0300

    noice remove

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -207,13 +207,5 @@ return require('packer').startup(function()
     }
   }
 
-  use({
-    "folke/noice.nvim",
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-      }
-  })
-
   -- END
 end)
