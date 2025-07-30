552249a3b00d6fba1b52e60010cefd9b22002be9
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-07-30 08:10:36 -0300

    file manager close

--- plugin/files.lua
+++ plugin/files.lua
@@ -1,2 +1,13 @@
-require("fyler").setup({})
+require("fyler").setup({
+  mappings = {
+    explorer = {
+      ["<c-b>"] = "CloseView",
+      ["<CR>"] = "Select",
+    },
+    confirm = {
+      ["y"] = "Confirm",
+      ["n"] = "Discard",
+    },
+  },
+})
 vim.keymap.set("n", "<c-b>", "<CMD>Fyler kind=split_left<CR>", { desc = "Open parent directory" })
