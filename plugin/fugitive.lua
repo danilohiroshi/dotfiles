933442c8f7763ee1c2cca74c5a91e025e5905270
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-08-06 15:17:43 -0300

    Add hints to fugitive

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_set_keymap("n", "<c-g>", ':G<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "<c-g>", ':G<CR>:echo "Hints: cc (commit), - (stage/unstage), x (discard changes), U (Unstage all), dd (show diff)"<CR>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap("v", "<c-g>", '<esc>:G<CR>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap("i", "<c-g>", '<esc>:G<CR>', {noremap=true, silent=true})
