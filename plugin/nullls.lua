b9a3d0f8e71102c26a741e207fc1e9aaaacb6968
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-08-07 11:28:18 -0300

    null_ls updates

--- plugin/nullls.lua
+++ plugin/nullls.lua
@@ -3,39 +3,31 @@ local null_ls = require("null-ls")
 local on_attach = function(client)
   if client.server_capabilities.document_formatting then
       vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
- end
+  end
 end
 
--- - Supported linters: https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local function is_javascript_or_typescript_project()
+  local js_ts_files = vim.fn.glob("*.js,*.jsx,*.ts,*.tsx", false, true)
+  local eslint_config = vim.fn.glob(".eslintrc*,eslint.config.js", false, true)
+  return #js_ts_files > 0 or #eslint_config > 0
+end
+
+local sources = {
+  null_ls.builtins.formatting.prettierd,
+}
+
+if is_javascript_or_typescript_project() then
+  table.insert(sources, require("none-ls.diagnostics.eslint"))
+  table.insert(sources, null_ls.builtins.code_actions.eslint)
+end
+
+if is_javascript_or_typescript_project() then
+  table.insert(sources, null_ls.builtins.diagnostics.tsc)
+end
+
 null_ls.setup({
-    sources = {
-      -- null_ls.builtins.diagnostics.cspell.with({
-      --   extra_args = { "--config", "~/.config/nvim/cspell/cspell.json" },
-      --   config = {
-      --       find_json = function(cwd)
-      --         return vim.fn.resolve(os.getenv('HOME') .. "/.config/nvim/cspell/cspell.json")
-      --       end
-      --   },
-      -- }),
-      -- null_ls.builtins.code_actions.cspell.with({
-      --   config = {
-      --       find_json = function(cwd)
-      --         return vim.fn.resolve(os.getenv('HOME') .. "/.config/nvim/cspell/cspell.json")
-      --       end
-      --   },
-      -- }),
-      require("none-ls.diagnostics.eslint"),
-      null_ls.builtins.code_actions.eslint,
-      null_ls.builtins.formatting.prettierd,
-      null_ls.builtins.diagnostics.tsc,
-      -- null_ls.builtins.diagnostics.phpcs.with({
-      --   extra_args = {
-      --       "--standard=Drupal,DrupalPractice",
-      --       "--extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml",
-      --     }
-      -- }),
-    },
-    on_attach = on_attach,
+  sources = sources,
+  on_attach = on_attach,
 })
 
 require('mason-null-ls').setup({ automatic_installation = true })
@@ -44,5 +36,6 @@ local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap("n", "<Leader>f", ':lua vim.lsp.buf.format()<cr>', opts)
 vim.api.nvim_set_keymap("i", "<Leader>f", '<esc>:lua vim.lsp.buf.format()<cr>', opts)
 vim.api.nvim_set_keymap("v", "<Leader>f", '<esc>:lua vim.lsp.buf.format()<cr>', opts)
+
 -- Format on save
 vim.cmd [[autocmd BufWritePre *{ts,tsx} lua vim.lsp.buf.format()]]
