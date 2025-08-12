c7da98d8fb345015452ad495fb66b3ffd87d9d17
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-08-12 10:14:03 -0300

    Open remote url and copy relative path

--- lua/globals/commands.lua
+++ lua/globals/commands.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_user_command('CopyRelPath', function()
   vim.fn.setreg('+', vim.fn.expand('%'))
 end, {})
 
-vim.api.nvim_create_user_command('OpenRemoteURL', function()
+vim.api.nvim_create_user_command('OpenRemoteURLAndCopyRelPath', function()
   -- Get the remote origin URL using Git command
   local handle = io.popen('git config --get remote.origin.url', 'r')
   local remote_url = handle:read('*a')
@@ -21,4 +21,8 @@ vim.api.nvim_create_user_command('OpenRemoteURL', function()
   os.execute('xdg-open ' .. remote_url)  -- For Linux
   -- os.execute('open ' .. remote_url)   -- For macOS
   -- os.execute('start ' .. remote_url)  -- For Windows
+
+  -- Copy the current buffer's relative path to the clipboard
+  local relative_path = vim.fn.expand('%')
+  vim.fn.setreg('+', relative_path)
 end, {})
