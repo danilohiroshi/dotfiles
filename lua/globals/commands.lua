39aa961be35c3dc184ef684ec7f4e3e8e759c175
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-08-12 11:59:29 -0300

    Get SSM Parameter command

--- lua/globals/commands.lua
+++ lua/globals/commands.lua
@@ -26,3 +26,23 @@ vim.api.nvim_create_user_command('OpenRemoteURLAndCopyRelPath', function()
   local relative_path = vim.fn.expand('%')
   vim.fn.setreg('+', relative_path)
 end, {})
+
+vim.api.nvim_create_user_command('GetSSMParameter', function(args)
+  local parameter_name = args.args
+  if parameter_name == '' then
+    print('Error: No parameter name provided!')
+    return
+  end
+
+  -- Execute AWS CLI command with dynamic parameter name
+  local command = 'aws ssm get-parameters --with-decryption --names=' .. parameter_name
+  local handle = io.popen(command, 'r')
+  local result = handle:read('*a')
+  handle:close()
+
+  -- Output the result in Neovim
+  print(result)
+end, {
+  nargs = 1,  -- Specifies that the command takes one argument
+  complete = 'file',  -- Optional: Autocomplete feature, adjust as needed
+})
