90e71f100dabfdd6643da7c3c73bad31e6e945ee
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-08-12 10:11:43 -0300

    Open remote url

--- lua/globals/commands.lua
+++ lua/globals/commands.lua
@@ -2,3 +2,23 @@
 vim.api.nvim_create_user_command('CopyRelPath', function()
   vim.fn.setreg('+', vim.fn.expand('%'))
 end, {})
+
+vim.api.nvim_create_user_command('OpenRemoteURL', function()
+  -- Get the remote origin URL using Git command
+  local handle = io.popen('git config --get remote.origin.url', 'r')
+  local remote_url = handle:read('*a')
+  handle:close()
+  
+  -- Trim newline or any trailing space
+  remote_url = remote_url:gsub('%s+', '')
+
+  if remote_url == '' then
+    print('Error: No remote URL found!')
+    return
+  end
+
+  -- Open the URL in the default browser (adjust for your OS)
+  os.execute('xdg-open ' .. remote_url)  -- For Linux
+  -- os.execute('open ' .. remote_url)   -- For macOS
+  -- os.execute('start ' .. remote_url)  -- For Windows
+end, {})
