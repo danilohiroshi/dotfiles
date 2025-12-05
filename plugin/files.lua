dd9205a365346b5fc26303ca63a52a8e869899e7
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-12-05 12:11:33 -0300

    files

--- plugin/files.lua
+++ plugin/files.lua
@@ -1,11 +1,22 @@
--- Mini.files keymapping with toggle functionality
+-- Mini.files keymapping with toggle functionality at current file location
+-- but keeping the full project tree view
 vim.keymap.set("n", "<c-b>", function()
-  if not MiniFiles.close() then
-    MiniFiles.open()
-  end
-end, { desc = "Toggle mini.files" })
+  local MiniFiles = require("mini.files")
 
--- Optional: Open mini.files at current file's directory
+  -- Try to close first, if it returns false, then open at current file location
+  if not MiniFiles.close() then
+    local current_file = vim.api.nvim_buf_get_name(0)
+    if current_file ~= "" then
+      -- Open at current file but show full tree from project root
+      MiniFiles.open(current_file, false)
+    else
+      -- If no file, open at project root
+      MiniFiles.open()
+    end
+  end
+end, { desc = "Toggle mini.files at current file with full tree" })
+
+-- Optional: Keep the leader+e mapping for consistency
 vim.keymap.set("n", "<leader>e", function()
   require("mini.files").open(vim.api.nvim_buf_get_name(0))
 end, { desc = "Open mini.files at current file" })
