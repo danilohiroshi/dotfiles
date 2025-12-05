1ab9f2b4f606d7dc960e15cf4e4cfeea005d3be4
Yamashita, Danilo <dyamash2@its.jnj.com>
2025-12-05 10:55:58 -0300

    LSP Config

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -1,53 +1,103 @@
 local util = require('lspconfig.util')
--- vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting_seq_sync, {})
 
-vim.keymap.set('n', '<leader>ca', ':CodeActionMenu<CR>')
-vim.keymap.set('v', '<leader>ca', ':CodeActionMenu<CR>')
-vim.keymap.set('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-
--- vim.lsp.set_log_level("debug")
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
-vim.keymap.set('n', '<c-[>', '<cmd>1po<CR>')
-
-local on_attach = function(_, bufnr)
-  vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { buffer = bufnr })
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = bufnr })
-  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = bufnr })
-  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { buffer = bufnr })
-  vim.keymap.set('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { buffer = bufnr })
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = bufnr })
-  vim.keymap.set('n', 'gr', "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = bufnr })
-end
-
--- nvim-cmp supports additional completion capabilities
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-require('lspconfig').html.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+-- Configuração de diagnósticos
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',
+    source = "if_many",
+  },
+  float = {
+    source = "always",
+    border = "rounded",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
 })
 
-require('lspconfig').intelephense.setup({
+-- Ícones de diagnóstico
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Keymaps globais
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Action" })
+vim.keymap.set('v', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Action" })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Show Diagnostics" })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Diagnostic List" })
+
+-- Função on_attach
+local on_attach = function(client, bufnr)
+  local opts = { buffer = bufnr, silent = true }
+
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, vim.tbl_extend('force', opts, { desc = "Go to Declaration" }))
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, vim.tbl_extend('force', opts, { desc = "Go to Definition" }))
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, vim.tbl_extend('force', opts, { desc = "Hover Documentation" }))
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, vim.tbl_extend('force', opts, { desc = "Go to Implementation" }))
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts, { desc = "Signature Help" }))
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, vim.tbl_extend('force', opts, { desc = "Type Definition" }))
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, vim.tbl_extend('force', opts, { desc = "Rename" }))
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, vim.tbl_extend('force', opts, { desc = "References" }))
+  vim.keymap.set('n', '<leader>f', function()
+    vim.lsp.buf.format { async = true }
+  end, vim.tbl_extend('force', opts, { desc = "Format" }))
+
+  -- Highlight do símbolo sob o cursor
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      group = "lsp_document_highlight",
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      group = "lsp_document_highlight",
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
+end
+
+-- Capabilities com nvim-cmp
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Configuração usando a nova API vim.lsp.config
+vim.lsp.config('lua_ls', {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      diagnostics = { globals = { 'vim' } },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = { enable = false },
+    },
+  },
+})
+
+vim.lsp.config('intelephense', {
+  cmd = { 'intelephense', '--stdio' },
+  filetypes = { 'php' },
+  root_markers = { 'composer.json', '.git' },
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
     intelephense = {
       files = {
-        maxSize = "10000000000",
-        associations = {
-          "*.php",
-          "*.phtml",
-          "*.inc",
-          "*.module",
-          "*.install",
-          "*.theme"
-        },
+        maxSize = 10000000,
+        associations = { "*.php", "*.phtml", "*.inc", "*.module", "*.install", "*.theme" },
         exclude = {
           "**/.git/**",
           "**/.svn/**",
@@ -55,7 +105,6 @@ require('lspconfig').intelephense.setup({
           "**/CVS/**",
           "**/.DS_Store/**",
           "**/node_modules/**",
-          "**/files/**",
           "**/bower_components/**",
           "**/vendor/**/{Tests,tests}/**",
           "**/.history/**",
@@ -64,12 +113,74 @@ require('lspconfig').intelephense.setup({
       phpdoc = {
         useFullyQualifiedNames = true,
         returnVoid = false
+      },
+      completion = {
+        fullyQualifyGlobalConstantsAndFunctions = true,
       }
     }
   }
 })
 
-require('lspconfig').ts_ls.setup({
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
+  root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = 'all',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      }
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayParameterNameHints = 'all',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      }
+    }
+  }
+})
+
+vim.lsp.config('html', {
+  cmd = { 'vscode-html-language-server', '--stdio' },
+  filetypes = { 'html' },
+  root_markers = { 'package.json', '.git' },
   on_attach = on_attach,
   capabilities = capabilities,
 })
+
+vim.lsp.config('cssls', {
+  cmd = { 'vscode-css-language-server', '--stdio' },
+  filetypes = { 'css', 'scss', 'less' },
+  root_markers = { 'package.json', '.git' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+vim.lsp.config('jsonls', {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+  root_markers = { 'package.json', '.git' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- Habilitar os servidores LSP automaticamente
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('intelephense')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('html')
+vim.lsp.enable('cssls')
+vim.lsp.enable('jsonls')
--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,7 +1,26 @@
-require("mason").setup()
-require("mason-lspconfig").setup {
-  ensure_installed = { "intelephense", "ts_ls" },
-}
+require("mason").setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
+})
+
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "intelephense",
+    "ts_ls",
+    "html",
+    "cssls",
+    "jsonls"
+  },
+  automatic_installation = true,
+})
+
 require("mason-nvim-dap").setup({
-  ensure_installed = { "node2", "js-debug-adapter", "php-debug-adapter" }, -- Automatically install the Node.js adapter
+  ensure_installed = { "node2", "js-debug-adapter", "php-debug-adapter" },
+  automatic_installation = true,
 })
